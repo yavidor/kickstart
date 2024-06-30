75d18cf5678c876bf5a156d0f37c56333963f373
yavidor <yonatan@avidor.org>
2024-06-30 12:36:34 +0300

    Remove inlay hints, no real reason

--- init.lua
+++ init.lua
@@ -529,11 +529,6 @@ require('lazy').setup({
               end,
             })
           end
-          if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
-            map('<leader>th', function()
-              vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-            end, '[T]oggle Inlay [H]ints')
-          end
         end,
       })
 
