fb8f554bfc2f2caba505194bd15670fd8594feb2
TJ DeVries <devries.timothyj@gmail.com>
2024-07-17 21:37:31 -0400

    fix: add required parsers from nvim-treesitter

--- init.lua
+++ init.lua
@@ -856,7 +856,7 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'go' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
