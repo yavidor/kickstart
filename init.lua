5d5648b01f6e187c8baea2874aa7180b7847d731
Rob <rcsfletcher@protonmail.com>
2025-02-18 00:12:06 +0000

    feat: add basic function signature help (#1358)
    * feat: add basic function signature help
    
    * Update init.lua
    
    Co-authored-by: makeworld <25111343+makew0rld@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: makeworld <25111343+makew0rld@users.noreply.github.com>

--- init.lua
+++ init.lua
@@ -783,6 +783,8 @@ require('lazy').setup({
       --  into multiple repos for maintenance purposes.
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
+      'hrsh7th/cmp-path',
+      'htsh7th/cmp-nvim-lsp-signature-help',
     },
     config = function()
       -- See `:help cmp`
@@ -859,6 +861,7 @@ require('lazy').setup({
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
           { name = 'path' },
+          { name = 'nvim_lsp_signature_help' },
         },
       }
     end,
